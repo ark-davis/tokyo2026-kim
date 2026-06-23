f83e4f5e28ff7df4cc76c6904e4c38a1359a9164
ark-davis <arkwithglobal.davis@gmail.com>
2026-06-23 09:38:24 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,10 +2,11 @@
    index.html(이미지·일정·아이콘 전부 내장)을 캐시에 저장해
    한 번 접속 후에는 인터넷 없이도 홈 화면 아이콘으로 실행됩니다. */
 
-const CACHE = 'tokyo-guide-v63';
+const CACHE = 'tokyo-guide-v67';
 const ASSETS = [
   './', './index.html', './sw.js',
-  './manifest.webmanifest', './icon-192.png', './icon-512.png'];
+  './manifest.webmanifest',
+  './icons/tokyo-180.png', './icons/tokyo-192.png', './icons/tokyo-512.png'];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
